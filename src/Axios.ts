c4044f254ac6dc2db86ffcac578c4f0fdb250105
Mejdi Ben Ammou <mejdi.ben.ammou@gmail.com>
2022-09-18 18:49:43 +0200

    deploy

--- src/Axios.ts
+++ src/Axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === "production" ? "https://head-book.herokuapp.com/api" : "http://localhost:5000/api"
+const baseUrl = process.env.NODE_ENV === "production" ? "https://headbook.adaptable.app/api" : "http://localhost:5000/api"
 
 export const Axios = axios.create({
     baseURL: baseUrl
